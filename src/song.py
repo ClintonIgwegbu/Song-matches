82781022b5d639daa0b7a851ebab4684d6ef4c2b
Clinton Igwegbu <clinton.igwegbu@gmail.com>
2020-03-06 17:04:16 +0000

    Prevent same song from being inserted in similarity graph

--- src/song.py
+++ src/song.py
@@ -1,3 +1,6 @@
+from bisect import insort
+from bisect import bisect_left
+
 class Song:
 
     def __init__(self, name, rating):
@@ -5,6 +8,25 @@ class Song:
         self.rating = rating
         self.similar_songs = []
 
+    # Used in bisect.insort to compare songs. Uppercase names come before lowercase names.
+    def __lt__(self, other):
+        return self.name < other.name
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    # TODO Prevent similar song from being added multiple times
     def add_similar_song(self, song):
-        self.similar_songs.append(song)
-        song.similar_songs.append(self)
+        # Insert similar songs into the list, sorted alphabetically.
+        if not self.similarity_already_noted(song):
+            insort(self.similar_songs, song)
+            insort(song.similar_songs, self)
+        # self.similar_songs.append(song)
+        # song.similar_songs.append(self)
+
+    def similarity_already_noted(self, x):
+        i = bisect_left(self.similar_songs, x)
+        if i != len(self.similar_songs) and self.similar_songs[i] == x:
+            return True
+        else:
+            return False
