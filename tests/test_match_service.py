f9a487656d72163843264a7c3df3e9db08f210cb
Clinton Igwegbu <clinton.igwegbu@gmail.com>
2020-03-07 01:24:51 +0000

    Tests made DRYer

--- tests/test_match_service.py
+++ tests/test_match_service.py
@@ -1,122 +1,82 @@
 import unittest
-# TODO: Should I import this class or is there a better way to include function definitions?
 from src.match_service import MatchService
 from src.song import Song
 
-# class TestMain(unittest.TestCase):
-    # These methods take input from the user, so definitely do need to be tested
-
-    # TODO for these functions, put multiple different input test cases in a file just as you tested in the terminal
-    # def test_do_song(inp):
-
-    # def test_do_similar(inp):
-
-    # def test_do_get_song_matches(inp):
-
-class TestSong(unittest.TestCase):
-
-    def test_add_similar_song(self):
-        song_a = Song('a', 1)
-        song_b = Song('b', 2)
-        song_c = Song('b', 3)
-        with self.subTest():
-            song_a.add_similar_song(song_b)
-            self.assertEqual(len(song_a.similar_songs), 1)
-        with self.subTest():
-            song_a.add_similar_song(song_b)
-            self.assertEqual(len(song_a.similar_songs), 1)  # Same song should not be added to graph twice
-        with self.subTest():
-            song_a.add_similar_song(song_c)
-            self.assertEqual(len(song_a.similar_songs), 2)
-
-
 class TestMatchService(unittest.TestCase):
 
-    def test_one_should_equal_one(self):
-        self.assertEqual(1, 1)
+    def reset_test_dfs(self, song):
+        num_top_rated_similar_songs = 1
+        top_rated_songs = [0] * num_top_rated_similar_songs
+        visited = {}
+        visited[song.name] = True
+        return num_top_rated_similar_songs, top_rated_songs, visited
 
-    # TODO Does this really have to be tested? It seems pretty high leve.
-    # def test_get_song_matches():
+
+    def assert_visited(self, conditions, songs, visited):
+        for i in range(6):
+            with self.subTest():
+                if conditions[i] == True:
+                    self.assertTrue(songs[i].name in visited)
+                else:
+                    self.assertFalse(songs[i].name in visited)
+
+    def setup_songs(self, a, b, c, d, e, f, g):
+        return Song('a', a), Song('b', b), Song('c', c), Song('d', d), Song('e', e), Song('f', f), Song('g', g)
 
 
-    # TODO How to take graph from input file?
     def test_dfs_similarity_graph(self):
-        # In below setup the grouped components of graph are {A,B,C,D}; {E,F}
-        song_dict = {}
-        song_dict['A'] = Song('A', 1.1)
-        song_dict['B'] = Song('B', 3.3)
-        song_dict['C'] = Song('C', 2.5)
-        song_dict['D'] = Song('D', 4.7)
-        song_dict['E'] = Song('D', 5.7)
-        song_dict['F'] = Song('D', 3.6)
-        song_dict['A'].add_similar_song(song_dict['B'])
-        song_dict['A'].add_similar_song(song_dict['C'])
-        song_dict['B'].add_similar_song(song_dict['D'])
-        song_dict['C'].add_similar_song(song_dict['D'])
-        song_dict['E'].add_similar_song(song_dict['F'])
+        # In below setup the grouped components of graph are {a,b,c,d}; {e,f}
+        song_a, song_b, song_c, song_d, song_e, song_f, _ = self.setup_songs(1.1, 3.3, 2.5, 4.7, 5.7, 3.6, 0)
+        song_a.add_similar_song(song_b)
+        song_a.add_similar_song(song_c)
+        song_b.add_similar_song(song_d)
+        song_c.add_similar_song(song_d)
+        song_e.add_similar_song(song_f)
+        songs = [song_a, song_b, song_c, song_d, song_e, song_f]
 
-        # TODO: Perhaps the white lines below could be called in separately or put in some kind of setup
-        # Check which nodes can be visited, starting from 'A'.
-        num_top_rated_similar_songs = 1
-        top_rated_songs = [0] * num_top_rated_similar_songs
-        visited = {}
-        visited['A'] = True
-        MatchService.dfs_similarity_graph(song_dict['A'], top_rated_songs, num_top_rated_similar_songs, song_dict, visited)
-        with self.subTest(song='A'):
-            self.assertTrue('A' in visited)
-        with self.subTest(song='B'):
-            self.assertTrue('B' in visited)
-        with self.subTest(song='C'):
-            self.assertTrue('C' in visited)
-        with self.subTest(song='D'):
-            self.assertTrue('D' in visited)
-        with self.subTest(song='E'):
-            self.assertFalse('E' in visited)
-        with self.subTest(song='F'):
-            self.assertFalse('F' in visited)
+        # Check which nodes can be visited, starting from song_a.
+        num_top_rated_similar_songs, top_rated_songs, visited = self.reset_test_dfs(song_a)
+        MatchService.dfs_similarity_graph(song_a, top_rated_songs, num_top_rated_similar_songs, visited)
+        self.assert_visited([True, True, True, True, False, False], songs, visited)
 
-        # Check which nodes can be visited, starting from 'E'
-        num_top_rated_similar_songs = 1
-        top_rated_songs = [0] * num_top_rated_similar_songs
-        visited = {}
-        visited['E'] = True
-        MatchService.dfs_similarity_graph(song_dict['E'], top_rated_songs, num_top_rated_similar_songs, song_dict, visited)
-        with self.subTest(song='A'):
-            self.assertFalse('A' in visited)
-        with self.subTest(song='B'):
-            self.assertFalse('B' in visited)
-        with self.subTest(song='C'):
-            self.assertFalse('C' in visited)
-        with self.subTest(song='D'):
-            self.assertFalse('D' in visited)
-        with self.subTest(song='E'):
-            self.assertTrue('E' in visited)
-        with self.subTest(song='F'):
-            self.assertTrue('F' in visited)
+        # Check which nodes can be visited, starting from song_e
+        num_top_rated_similar_songs, top_rated_songs, visited = self.reset_test_dfs(song_e)
+        MatchService.dfs_similarity_graph(song_e, top_rated_songs, num_top_rated_similar_songs, visited)
+        self.assert_visited([False, False, False, False, True, True], songs, visited)
 
-        # In below setup the gouped components of graph are {A,B,C,D,E,F};
-        # Check which nodes can be visited, starting from 'E'
-        song_dict['F'].add_similar_song(song_dict['B'])
-        num_top_rated_similar_songs = 1
-        top_rated_songs = [0] * num_top_rated_similar_songs
-        visited = {}
-        visited['E'] = True
-        MatchService.dfs_similarity_graph(song_dict['E'], top_rated_songs, num_top_rated_similar_songs, song_dict, visited)
-        with self.subTest(song='A'):
-            self.assertTrue('A' in visited)
-        with self.subTest(song='B'):
-            self.assertTrue('B' in visited)
-        with self.subTest(song='C'):
-            self.assertTrue('C' in visited)
-        with self.subTest(song='D'):
-            self.assertTrue('D' in visited)
-        with self.subTest(song='E'):
-            self.assertTrue('E' in visited)
-        with self.subTest(song='F'):
-            self.assertTrue('F' in visited)
+        # In below setup the grouped components of graph are {a,b,c,d,e,f};
+        song_f.add_similar_song(song_b)
+        # Check which nodes can be visited, starting from song_e
+        num_top_rated_similar_songs, top_rated_songs, visited = self.reset_test_dfs(song_e)
+        MatchService.dfs_similarity_graph(song_e, top_rated_songs, num_top_rated_similar_songs, visited)
+        self.assert_visited([True, True, True, True, True, True], songs, visited)
 
-    # TODO
-    # def test_update_top_rated():
+
+    def test_update_top_rated(self):
+        song_a, song_b, song_c, song_d, song_e, song_f, song_g = self.setup_songs(1, 2, 3, 0, 1.1, 2.1, 3.1)
+        num_top_rated_similar_songs = 3
+        top_rated_songs_before = [song_a, song_b, song_c]
+
+        top_rated_songs_after = top_rated_songs_before[:]
+        MatchService.update_top_rated(song_d, top_rated_songs_after, num_top_rated_similar_songs)
+        with self.subTest():
+            self.assertEqual(top_rated_songs_after, [song_a, song_b, song_c])
+
+        top_rated_songs_after = top_rated_songs_before[:]
+        MatchService.update_top_rated(song_e, top_rated_songs_after, num_top_rated_similar_songs)
+        with self.subTest():
+            self.assertEqual(top_rated_songs_after, [song_e, song_b, song_c])
+
+        top_rated_songs_after = top_rated_songs_before[:]
+        MatchService.update_top_rated(song_f, top_rated_songs_after, num_top_rated_similar_songs)
+        with self.subTest():
+            self.assertEqual(top_rated_songs_after, [song_b, song_f, song_c])
+
+        top_rated_songs_after = top_rated_songs_before[:]
+        MatchService.update_top_rated(song_g, top_rated_songs_after, num_top_rated_similar_songs)
+        with self.subTest():
+            self.assertEqual(top_rated_songs_after, [song_b, song_c, song_g])
+
 
     def test_shift_and_update(self):
         top_rated_songs = ['a', 'b', 'c']
@@ -134,7 +94,7 @@ class TestMatchService(unittest.TestCase):
         with self.subTest(i=2):
             self.assertEqual(top_rated_songs, ['b', 'c', 'd'])
 
-    # TODO How to setup/teardown test methods and change a test case variable rather than repeating code?
+
     def test_remove_empty_spaces(self):
         # NOTE: Input functions should not allow negative test values to get through
         test_values = [0, 4]
